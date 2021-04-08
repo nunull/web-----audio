95b5cbc0dab904d0df058b14dd055d53ff36b9c4
Timm Albers <timmm.albers@gmail.com>
2021-04-08 17:36:37 +0200

    vuejs

---
+++ audio.js
@@ -0,0 +1,196 @@
+const audio = {
+  context: null,
+  reverbNode: null,
+  droneGainLeft: null,
+  droneGainRight: null,
+  audioPlayerGain: null,
+  voices: [],
+  
+  setAttack (value) {
+    this.voices.forEach(voice => {
+      voice.attackValue = value
+    })
+  },
+
+  setRelease (value) {
+    this.voices.forEach(voice => {
+      voice.releaseValue = value
+    })
+  },
+  
+  init (voices) {
+    if (this.context) {
+      this.context.resume()
+      return
+    }
+    
+    console.log('use `audio.setAttack(value)`')
+    console.log('use `audio.setRelease(value)`')
+    
+    console.log('init')
+    
+    this.context = new (window.AudioContext || window.webkitAudioContext)()
+    reverbjs.extend(this.context);
+    
+    const reverbUrl = "http://reverbjs.org/Library/LadyChapelStAlbansCathedral.m4a";
+    this.reverbNode = this.context.createReverbFromUrl(reverbUrl, () => {
+      this.reverbNode.connect(this.context.destination)
+      
+      app.onInitialized()
+    })
+    
+    const left = this.context.createStereoPanner()
+    const right = this.context.createStereoPanner()
+    left.pan.setValueAtTime(-0.5, this.context.currentTime)
+    right.pan.setValueAtTime(0.5, this.context.currentTime)
+    
+    left.connect(this.reverbNode)
+    right.connect(this.reverbNode)
+    
+    const lfos = [
+      this.createLfo(0.12),
+      this.createLfo(0.23),
+      this.createLfo(4)
+    ]
+    
+    const modIndices = {
+      all: this.context.createGain(),
+      odd: this.context.createGain(),
+      even: this.context.createGain(),
+      left: this.context.createGain(),
+      right: this.context.createGain()
+    }
+    
+    lfos[0].createGain(1000).connect(modIndices.all.gain)
+    lfos[1].createGain(1000).connect(modIndices.odd.gain)
+    lfos[2].createGain(1000).connect(modIndices.even.gain)
+    lfos[0].createGain(600).connect(modIndices.left.gain)
+    lfos[1].createGain(600).connect(modIndices.right.gain)
+    
+    droneGainLeft = this.context.createGain()
+    droneGainLeft.gain.setValueAtTime(0.8, this.context.currentTime)
+    droneGainLeft.connect(left)
+    
+    droneGainRight = this.context.createGain()
+    droneGainRight.gain.setValueAtTime(0.8, this.context.currentTime)
+    droneGainRight.connect(right)
+    
+    audioPlayerGain = this.context.createGain()
+    audioPlayerGain.gain.setValueAtTime(0.8, this.context.currentTime)
+    audioPlayerGain.connect(this.reverbNode)
+
+    for (let i = 0; i < app.drone.voices.length; i++) {
+      const voice_ = app.drone.voices[i]
+      if (!voice_.key) continue
+      
+      const voice = this.createVoice(voice_.key, voice_.note)
+      this.voices.push(voice)
+      
+      voice.gain.connect(i % 2 == 0 ? droneGainLeft : droneGainRight)
+      
+      voice.gain.connect(modIndices.all)
+      modIndices.all.connect(voice.oscillator.detune)
+      
+      if (i % 2 == 0) {
+        voice.gain.connect(modIndices.even)
+        modIndices.even.connect(voice.oscillator.detune)
+      } else {
+        voice.gain.connect(modIndices.odd)
+        modIndices.odd.connect(voice.oscillator.detune)
+      }
+      
+      if (i < 4) {
+        voice.gain.connect(modIndices.left)
+        modIndices.left.connect(voice.oscillator.detune)
+      } else {
+        voice.gain.connect(modIndices.right)
+        modIndices.right.connect(voice.oscillator.detune)
+      }
+    }
+  },
+  
+  playFile (buffer) {
+    const sourceNode = audio.context.createBufferSource()
+    sourceNode.buffer = buffer
+    sourceNode.loop = true
+    sourceNode.connect(audioPlayerGain)
+    sourceNode.start()
+    
+    return sourceNode
+  },
+  
+  onParamChange (param) {
+    const droneVolume = app.drone.params.find(param => param.name === 'volume')
+    // const reverb = app.drone.params.find(param => param.name === 'reverb')
+    // const tone = app.drone.params.find(param => param.name === 'volume')
+    
+    droneGainLeft.gain.linearRampToValueAtTime(droneVolume.value, this.context.currentTime + 0.2)
+    droneGainRight.gain.linearRampToValueAtTime(droneVolume.value, this.context.currentTime + 0.2)
+    
+    const audioPlayerVolume = app.audioPlayer.params.find(param => param.name === 'volume')
+    
+    audioPlayerGain.gain.linearRampToValueAtTime(audioPlayerVolume.value, this.context.currentTime + 0.2)
+  },
+  
+  // createGain (value) {
+  //   const gain = this.context.createGain()
+  //   gain.setValueAtTime(value, this.context.currentTime)
+  //   return gain
+  // },
+  
+  createLfo (frequency) {
+    const context = this.context
+    const oscillator = context.createOscillator()
+    oscillator.type = 'sine'
+    oscillator.frequency.setValueAtTime(frequency, this.context.currentTime)
+    oscillator.start()
+    
+    return {
+      createGain (value) {
+        const gain = context.createGain()
+        gain.gain.setValueAtTime(value, context.currentTime)
+        oscillator.connect(gain)
+        return gain
+      }
+    }
+  },
+
+  createVoice (key, note) {
+    const frequency = Tonal.Note.freq(note)
+    console.log(frequency)
+    
+    const gain = this.context.createGain()
+    gain.gain.setValueAtTime(0, this.context.currentTime)
+    
+    const oscillator = this.context.createOscillator()
+    oscillator.type = 'sine'
+    oscillator.frequency.setValueAtTime(frequency, this.context.currentTime)
+    oscillator.start()
+    
+    oscillator.connect(gain)
+    
+    // TODO
+    const context = this.context
+    
+    return {
+      attackValue: 3,
+      releaseValue: 3,
+      key,
+      note,
+      gain,
+      oscillator,
+      attack () {
+        // gain.gain.cancelAndHoldAtTime(context.currentTime)
+        gain.gain.cancelScheduledValues(context.currentTime)
+        gain.gain.linearRampToValueAtTime(0, context.currentTime + 0.01)
+        gain.gain.linearRampToValueAtTime(0.125, context.currentTime + this.attackValue)
+      },
+      release () {
+        // gain.gain.cancelAndHoldAtTime(context.currentTime)
+        gain.gain.cancelScheduledValues(context.currentTime)
+        gain.gain.linearRampToValueAtTime(0, context.currentTime + this.releaseValue)
+      }
+    }
+  }
+}
+
--- index.js
+++ index.js
@@ -1,270 +1,230 @@
-const keys = {
-  'a': 'c3',
-  's': 'c4',
-  'd': 'd#4',
-  'f': 'g4',
-  'j': 'g#4',
-  'k': 'f3',
-  'l': 'g#3',
-  ';': 'c5'
-}
+const app = new Vue({
+  el: '#app',
+  data: {
+    drone: {
+      params: [{
+        module: 'drone',
+        name: 'volume',
+        value: 0.8,
+        active: true
+      // }, {
+      //   module: 'drone',
+      //   name: 'reverb',
+      //   value: 0.8,
+      //   active: false
+      // }, {
+      //   module: 'drone',
+      //   name: 'tone',
+      //   value: 0.2,
+      //   active: false
+      }],
+      voices: [{
+        key: 'a',
+        note: 'c3',
+        active: false
+      }, {
+        key: 's',
+        note: 'c4',
+        active: false
+      }, {
+        key: 'd',
+        note: 'd#4',
+        active: false
+      }, {
+        key: 'f',
+        note: 'g4',
+        active: false
+      }, {
+        
+      }, {
+        key: 'j',
+        note: 'g#4',
+        active: false
+      }, {
+        key: 'k',
+        note: 'f3',
+        active: false
+      }, {
+        key: 'l',
+        note: 'g#3',
+        active: false
+      }, {
+        key: ';',
+        note: 'c5',
+        active: false
+      }]
+    },
+    audioPlayer: {
+      params: [{
+        module: 'audioPlayer',
+        name: 'volume',
+        value: 0.8,
+        active: false
+      // }, {
+      //   module: 'audioPlayer',
+      //   name: 'reverb',
+      //   value: 0.8,
+      //   active: false
+      }],
+      files: []
+    },
+    audio: {
+      initialized: false
+    },
+    modal: {
+      name: null
+    }
+  },
+  methods: {
+    init () {
+      if (!app.audio.initialized) this.showModal('initializing')
+      
+      audio.init()
+    },
+    
+    onInitialized () {
+      app.audio.initialized = true
+      this.hideModal()
+    },
+    
+    playVoice (key) {
+      if (!app.audio.initialized) return  
+      
+      const voice_ = this.drone.voices.find(v => v.key === key)
+      if (!voice_) return
+      
+      const voice = audio.voices.find(v => v.key === key)
+      
+      voice_.active = true
+      voice.attack()
+    },
+    
+    stopVoice (key) {
+      if (!app.audio.initialized) return
+      
+      const voice_ = this.drone.voices.find(v => v.key === key)
+      if (!voice_) return
+      
+      const voice = audio.voices.find(v => v.key === key)
+      
+      voice_.active = false
+      voice.release()
+    },
+    
+    playFile (file) {
+      if (!app.audio.initialized) return
+      
+      if (!file.active) {
+        console.log('playing', file.name)
+        
+        file.sourceNode = audio.playFile(file.audioBuffer)
+        file.active = true
+      } else {
+        console.log('stopping', file.name)
+        
+        file.sourceNode.stop()
+        file.active = false
+      }
+    },
+    
+    selectNextParam () {
+      const params = this.drone.params.concat(this.audioPlayer.params)
+      const currentParam = params.find(param => param.active)
+      const currentIndex = params.indexOf(currentParam)
+      const nextIndex = (currentIndex + 1) % params.length
+      const nextParam = params[nextIndex]
+      
+      currentParam.active = false
+      nextParam.active = true
+    },
+    
+    increaseCurrentParam () {
+      const params = this.drone.params.concat(this.audioPlayer.params)
+      const currentParam = params.find(param => param.active)
+      
+      currentParam.value = Math.min(1, currentParam.value + 0.1)
+      currentParam.value = Math.round(currentParam.value * 10) / 10
+      
+      audio.onParamChange(currentParam)
+    },
+    
+    decreaseCurrentParam () {
+      const params = this.drone.params.concat(this.audioPlayer.params)
+      const currentParam = params.find(param => param.active)
+      
+      currentParam.value = Math.max(0, currentParam.value - 0.1)
+      currentParam.value = Math.round(currentParam.value * 10) / 10
+      
+      audio.onParamChange(currentParam)
+    },
+    
+    showModal (name) {
+      this.modal.name = name
+    },
+    
+    hideModal () {
+      this.modal.name = null
+    }
+  }
+})
 
-let initialized = false
-let audioContext
-let reverbNode
-let audioFiles = []
-const voices = []
-
-// ideas:
-// - gain for each module
-// - drone parameters
-// - looper
-// - recording
-
-render()
+app.showModal('start')
 
 document.addEventListener('keydown', event => {
-  if (event.repeat) return
-  
-  console.log('keydown', event)
-  if (!initialized) {
-    init()
-    return
+  switch (event.key) {
+    case 'Tab':
+      event.preventDefault()
+      app.selectNextParam()
+      return
+    case 'ArrowUp':
+    case 'ArrowRight':
+      event.preventDefault()
+      app.increaseCurrentParam()
+      return
+    case 'ArrowDown':
+    case 'ArrowLeft':
+      event.preventDefault()
+      app.decreaseCurrentParam()
+      return
   }
   
-  const voice = voices.find(v => v.key === event.key)
-  if (voice) voice.attack()
+  if (event.repeat) return
   
-  render()
+  app.init()
+
+  app.playVoice(event.key)
 })
 
 document.addEventListener('keyup', event => {
-  const voice = voices.find(v => v.key === event.key)
-  if (voice) voice.release()
-  
-  render()
+  app.stopVoice(event.key)
 })
 
-const dropElement = document.querySelector('#drop')
-
-dropElement.addEventListener('dragover', event => {
+document.addEventListener('dragover', event => {
   event.preventDefault()
-  dropElement.classList.add('active')
+  app.showModal('drop')
 })
 
-dropElement.addEventListener('drop', async event => {
+document.addEventListener('drop', async event => {
   console.log('drop', event)
   
   event.preventDefault()
-  dropElement.classList.remove('active')
-  
-  init()
+  app.hideModal()
+  app.init()
   
   const audioItems = Array.from(event.dataTransfer.items)
-    .filter(item =>
-      item.kind === 'file' && item.type.indexOf('audio/') === 0
-    )
+    .filter(item => item.kind === 'file' && item.type.indexOf('audio/') === 0)
   
   const newAudioFiles = await Promise.all(audioItems.map(async item => {
     const file = item.getAsFile()
     const buffer = await file.arrayBuffer()
-    const audioBuffer = await audioContext.decodeAudioData(buffer)
+    const audioBuffer = await audio.context.decodeAudioData(buffer)
+    
     return {
       name: file.name,
-      audioBuffer
+      audioBuffer,
+      active: false
     }
   }))
   
-  audioFiles = audioFiles.concat(newAudioFiles)
-    
-  render()
+  app.audioPlayer.files = app.audioPlayer.files.concat(newAudioFiles)
 })
-
-document.querySelector('#audio-files').addEventListener('click', event => {
-  if (!event.target.classList.contains('audio-file')) return
-  
-  playAudioFile(event.target.innerText)
-})
-
-function init () {
-  if (audioContext) return
-  
-  console.log('init')
-  
-  audioContext = new (window.AudioContext || window.webkitAudioContext)()
-  reverbjs.extend(audioContext);
-  
-  const reverbUrl = "http://reverbjs.org/Library/LadyChapelStAlbansCathedral.m4a";
-  reverbNode = audioContext.createReverbFromUrl(reverbUrl, function() {
-    reverbNode.connect(audioContext.destination)
-    
-    initialized = true
-    render()
-  })
-  
-  const left = audioContext.createStereoPanner()
-  const right = audioContext.createStereoPanner()
-  left.pan.setValueAtTime(-0.5, audioContext.currentTime)
-  right.pan.setValueAtTime(0.5, audioContext.currentTime)
-  
-  left.connect(reverbNode)
-  right.connect(reverbNode)
-  
-  const lfos = [
-    createLfo(0.12),
-    createLfo(0.23),
-    createLfo(4)
-  ]
-  
-  const modIndices = {
-    all: audioContext.createGain(),
-    odd: audioContext.createGain(),
-    even: audioContext.createGain(),
-    left: audioContext.createGain(),
-    right: audioContext.createGain()
-  }
-  
-  lfos[0].createGain(1000).connect(modIndices.all.gain)
-  lfos[1].createGain(1000).connect(modIndices.odd.gain)
-  lfos[2].createGain(1000).connect(modIndices.even.gain)
-  lfos[0].createGain(600).connect(modIndices.left.gain)
-  lfos[1].createGain(600).connect(modIndices.right.gain)
-
-  const keyNotes = Object.entries(keys)
-  for (let i = 0; i < keyNotes.length; i++) {
-    const [key, note] = keyNotes[i]
-    const voice = createVoice(key, note)
-    voices.push(voice)
-    
-    voice.gain.connect(i % 2 == 0 ? left : right)
-    
-    voice.gain.connect(modIndices.all)
-    modIndices.all.connect(voice.oscillator.detune)
-    
-    if (i % 2 == 0) {
-      voice.gain.connect(modIndices.even)
-      modIndices.even.connect(voice.oscillator.detune)
-    } else {
-      voice.gain.connect(modIndices.odd)
-      modIndices.odd.connect(voice.oscillator.detune)
-    }
-    
-    if (i < 4) {
-      voice.gain.connect(modIndices.left)
-      modIndices.left.connect(voice.oscillator.detune)
-    } else {
-      voice.gain.connect(modIndices.right)
-      modIndices.right.connect(voice.oscillator.detune)
-    }
-  }
-}
-
-function createLfo (frequency) {
-  const oscillator = audioContext.createOscillator()
-  oscillator.type = 'sine'
-  oscillator.frequency.setValueAtTime(frequency, audioContext.currentTime)
-  oscillator.start()
-  
-  return {
-    createGain (value) {
-      const gain = audioContext.createGain()
-      gain.gain.setValueAtTime(value, audioContext.currentTime)
-      oscillator.connect(gain)
-      return gain
-    }
-  }
-}
-
-function createVoice (key, note) {
-  const frequency = Tonal.Note.freq(note)
-  console.log(frequency)
-  
-  const gain = audioContext.createGain()
-  gain.gain.setValueAtTime(0, audioContext.currentTime)
-  
-  const oscillator = audioContext.createOscillator()
-  oscillator.type = 'sine'
-  oscillator.frequency.setValueAtTime(frequency, audioContext.currentTime)
-  oscillator.start()
-  
-  oscillator.connect(gain)
-  
-  return {
-    key,
-    note,
-    gain,
-    oscillator,
-    active: false,
-    attack () {
-      this.active = true
-      gain.gain.cancelAndHoldAtTime(audioContext.currentTime)
-      gain.gain.linearRampToValueAtTime(0, audioContext.currentTime + 0.01)
-      gain.gain.linearRampToValueAtTime(0.125, audioContext.currentTime + 5)
-    },
-    release () {
-      this.active = false
-      gain.gain.cancelAndHoldAtTime(audioContext.currentTime)
-      gain.gain.linearRampToValueAtTime(0, audioContext.currentTime + 3)
-    }
-  }
-}
-
-function playAudioFile (name) {
-  const audioFile = audioFiles.find(file => file.name === name)
-  if (!audioFile) return
-  
-  if (!audioFile.active) {
-    console.log('playing', name)
-    
-    const sourceNode = audioContext.createBufferSource()
-    sourceNode.buffer = audioFile.audioBuffer
-    sourceNode.loop = true
-    sourceNode.connect(reverbNode)
-    sourceNode.start()
-    
-    audioFile.sourceNode = sourceNode
-    audioFile.active = true
-  } else {
-    audioFile.sourceNode.stop()
-    audioFile.active = false
-  }
-  
-  render()
-}
-
-function render () {
-  const droneValuesElement = document.querySelector('#drone-values')
-  const voicesElement = document.querySelector('#voices')
-  
-  const audioPlayerValuesElement = document.querySelector('#audio-player-values')
-  const audioFilesElement = document.querySelector('#audio-files')
-  
-  if (initialized) voicesElement.classList.remove('deactivated')
-  
-  droneValuesElement.innerHTML = `<div class="value">volume: 0.8</div>`
-  
-  voicesElement.innerHTML = ''
-  for (let i = 0; i < Object.keys(keys).length; i++) {
-    const key = Object.keys(keys)[i]
-    const note = keys[key]
-    const voice = voices[i]
-    voicesElement.innerHTML += `
-      <div class="voice${(voice || {}).active ? ' active' : ''}">
-        <div>${key.toUpperCase()}</div>
-        <div class="note">${note}</div>
-      </div>`
-      
-    if (i === 3) {
-      voicesElement.innerHTML += `<div class="spacer"></div>`
-    }
-  }
-  
-  audioPlayerValuesElement.innerHTML = `<div class="value">volume: 0.8</div>`
-  
-  audioFilesElement.innerHTML = ''
-  for (const audioFile of audioFiles) {
-    audioFilesElement.innerHTML += `
-      <div class="audio-file${audioFile.active ? ' active' : ''}">${audioFile.name}
-      </div>`
-  }
-}
